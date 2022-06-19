a08fc7a32b20b612aaf1c113d27b6d9d5d42ca8c
KoalaAssasin <koalaassasinn@gmail.com>
2022-06-19 14:51:07 +1000

    Very Basic Collision
    -obstacle added to the scene
    -colliding with it makes a console message

---
+++ Glide Hoob/Assets/Scripts/CollisionTester.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Obstacle")
+        {
+            Debug.Log("smacked");
+        }
+    }
+
+}
